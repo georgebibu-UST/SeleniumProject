ee103bb57e94b1869577196255468d714c9f79ee
George Bibu(UST <310918@ust.com>
2026-07-10 09:18:15 +0530

    build fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,10 @@ tasks.withType<Test>().configureEach {
     systemProperty("browser", providers.gradleProperty("browser").orElse("chrome").get())
     systemProperty("build.label", providers.gradleProperty("buildLabel").orElse("gradle-local").get())
     systemProperty("cucumber.publish.quiet", "true")
+    systemProperty(
+        "allure.results.directory",
+        layout.buildDirectory.dir("allure-results").get().asFile.absolutePath
+    )
     testLogging {
         events("passed", "skipped", "failed")
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.SHORT
